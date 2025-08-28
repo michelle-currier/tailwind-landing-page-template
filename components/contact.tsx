cfad96a5249c6130929538c65cf486534613701d
michelle-currier <mushel@gmail.com>
2025-08-28 18:37:50 -0500

    the src was wrong changed it back

--- components/contact.tsx
+++ components/contact.tsx
@@ -23,7 +23,7 @@ export default function ContactComponent() {
     e.preventDefault();
 
     try {
-      const response = await fetch("/app/api/send-email", {
+      const response = await fetch("/api/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
